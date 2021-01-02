1e2857d387b38fc2854dc4f9f9fd3cb803f261d5
HUIKIM0 <fehs123@naver.com>
2021-01-02 21:33:27 +0900

    update

--- Array/Form1.Designer.cs
+++ Array/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace Array
+namespace ArrayTest
 {
     partial class Form1
     {
@@ -30,10 +30,7 @@ namespace Array
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.lblArrayCount = new System.Windows.Forms.Label();
+            this.dgDay = new System.Windows.Forms.DataGridView();
             this.colDay1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colDay2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colDay3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -41,7 +38,10 @@ namespace Array
             this.colDay5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colDay6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colDay7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblArrayCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDay)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -53,10 +53,10 @@ namespace Array
             this.label1.TabIndex = 0;
             this.label1.Text = "매장 방문 수";
             // 
-            // dataGridView1
+            // dgDay
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgDay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDay.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colDay1,
             this.colDay2,
             this.colDay3,
@@ -64,39 +64,12 @@ namespace Array
             this.colDay5,
             this.colDay6,
             this.colDay7});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 56);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(1062, 150);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 234);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 32);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "일주일간";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(107, 234);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 32);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "이주일간";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // lblArrayCount
-            // 
-            this.lblArrayCount.AutoSize = true;
-            this.lblArrayCount.Location = new System.Drawing.Point(200, 243);
-            this.lblArrayCount.Name = "lblArrayCount";
-            this.lblArrayCount.Size = new System.Drawing.Size(105, 15);
-            this.lblArrayCount.TabIndex = 0;
-            this.lblArrayCount.Text = "전체 자료 수:0";
+            this.dgDay.Location = new System.Drawing.Point(12, 56);
+            this.dgDay.Name = "dgDay";
+            this.dgDay.RowHeadersWidth = 51;
+            this.dgDay.RowTemplate.Height = 27;
+            this.dgDay.Size = new System.Drawing.Size(1062, 150);
+            this.dgDay.TabIndex = 1;
             // 
             // colDay1
             // 
@@ -147,6 +120,35 @@ namespace Array
             this.colDay7.Name = "colDay7";
             this.colDay7.Width = 125;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "일주일간";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "이주일간";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblArrayCount
+            // 
+            this.lblArrayCount.AutoSize = true;
+            this.lblArrayCount.Location = new System.Drawing.Point(200, 243);
+            this.lblArrayCount.Name = "lblArrayCount";
+            this.lblArrayCount.Size = new System.Drawing.Size(105, 15);
+            this.lblArrayCount.TabIndex = 0;
+            this.lblArrayCount.Text = "전체 자료 수:0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
@@ -154,12 +156,12 @@ namespace Array
             this.ClientSize = new System.Drawing.Size(1086, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgDay);
             this.Controls.Add(this.lblArrayCount);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDay)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,7 +170,7 @@ namespace Array
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgDay;
         private System.Windows.Forms.DataGridViewTextBoxColumn colDay1;
         private System.Windows.Forms.DataGridViewTextBoxColumn colDay2;
         private System.Windows.Forms.DataGridViewTextBoxColumn colDay3;
--- Array/Form1.cs
+++ Array/Form1.cs
@@ -8,13 +8,96 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Array
+namespace ArrayTest
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            ArrayTest();
+            ArrayClassTest();
+        }
+
+
+        private void ArrayTest()
+        {
+            //int iDay1, iDay2, iDay3;
+            //iDay1 = 10;
+            //iDay2 = 30;
+            //iDay3 = 25;
+
+            //string strT1 = "가,나,다,라,마,바,사";
+            //string[] strT1Split = strT1.Split(',');
+
+
+            int[] iArrayDay = { 1, 2, 3, 4, 5 };
+
+            int[] iArrayDay2 = new int[5] { 1, 2, 3, 4, 5 };
+
+            int[] iArrayDay3 = new int[5];
+            iArrayDay3[0] = 10;
+            iArrayDay3[1] = 20;
+            iArrayDay3[2] = 30;
+            iArrayDay3[3] = 40;
+            iArrayDay3[4] = 50;
+
+            int[,] iArrayDay4 = new int[2, 4] { { 1, 2, 3, 4 }, { 10, 20, 30, 40 } };
+        }
+
+        private void ArrayClassTest()
+        {
+            int[] iTest = { 10, 20, 30, 40, 50 };
+            int i = iTest.Length;
+
+            Array.Clear(iTest,2,2);  //2번째부터 2개를 0으로
+            Array.Resize(ref iTest, 10);  // 배열을 늘림
+
+            int iWhare = Array.IndexOf(iTest, 50);  //50이 어디에 있나
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] iTest = { 10, 23, 31, 29, 51, 62, 70 };
+
+            lblArrayCount.Text = String.Format("전체 자료 수:{0}",iTest.Length.ToString());
+
+            dgDay["colDay1", 0].Value = iTest[0];
+            dgDay["colDay2", 0].Value = iTest[1];
+            dgDay["colDay3", 0].Value = iTest[2];
+            dgDay["colDay4", 0].Value = iTest[3];
+            dgDay["colDay5", 0].Value = iTest[4];
+            dgDay["colDay6", 0].Value = iTest[5];
+            dgDay["colDay7", 0].Value = iTest[6];
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dgDay.Rows.Clear();  // 버튼을 아무리 눌러도 계속 생성되지X 초기화
+
+            int[,] iTest2 = { { 13, 20, 49, 39, 51, 33, 48 }, { 40, 32, 50, 61, 65, 59, 50 } };
+
+            lblArrayCount.Text = String.Format("전체 자료 수:{0}", iTest2.Length.ToString());
+
+            dgDay.Rows.Add();
+
+            dgDay["colDay1", 0].Value = iTest2[0,0];
+            dgDay["colDay2", 0].Value = iTest2[0,1];
+            dgDay["colDay3", 0].Value = iTest2[0,2];
+            dgDay["colDay4", 0].Value = iTest2[0,3];
+            dgDay["colDay5", 0].Value = iTest2[0,4];
+            dgDay["colDay6", 0].Value = iTest2[0,5];
+            dgDay["colDay7", 0].Value = iTest2[0,6];
+
+            dgDay["colDay1", 1].Value = iTest2[1,0];
+            dgDay["colDay2", 1].Value = iTest2[1,1];
+            dgDay["colDay3", 1].Value = iTest2[1,2];
+            dgDay["colDay4", 1].Value = iTest2[1,3];
+            dgDay["colDay5", 1].Value = iTest2[1,4];
+            dgDay["colDay6", 1].Value = iTest2[1,5];
+            dgDay["colDay7", 1].Value = iTest2[1,6];
         }
     }
 }
