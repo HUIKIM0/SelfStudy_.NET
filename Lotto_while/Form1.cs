6eb96c10c5ee71dd4258e8a594b19ab6e72ef824
HUIKIM0 <fehs123@naver.com>
2021-01-07 18:01:25 +0900

    update

--- Lotto_while/Form1.cs
+++ Lotto_while/Form1.cs
@@ -24,11 +24,41 @@ namespace Lotto_while
             int[] iArray = new int[6];
             int ArrayCount = 0;
 
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new StringBuilder();  
             Random rd = new Random();
 
+            while (Array.IndexOf(iArray,0) != -1)  //IndexOf는 찾는 값이 없으면 -1를 반환
+            {
+                // 반환값이 -1이 아니다 => 0이 존재한다. 계속진행
+                int iNumber = rd.Next(1, 46); // 1 <= x < 46
+
+                if (true)
+                {
+                    if (Array.IndexOf(iArray,iNumber) == -1)   //iArray안에 랜덤으로 찾은값(iNumber)가 존재하지 않으면(-1)
+                    {
+                        iArray[ArrayCount] = iNumber;
+                        //sb.Append(string.Format("{0}. ", iNumber));
+
+                        ArrayCount++;
 
 
+                    }
+
+                }
+
+            }
+
+            //배열 sort
+            Array.Sort(iArray);
+
+            foreach (int s in iArray)
+            {
+                sb.Append(string.Format("{0}. ",s));  // StringBuilder sb에 정렬된 배열값들 추가
+            }
+
+
+            lblrottoResult.Text = sb.ToString();  // string 형태로 바꿔서 출력
+            lboxrottoResult.Items.Add(sb.ToString());  //ListBox 값 넣기
 
         }
     }
