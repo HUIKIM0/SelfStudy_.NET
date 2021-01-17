e25b6081596844e2fae53f2b07c406fbbcda3865
HUIKIM0 <fehs123@naver.com>
2021-01-17 18:12:36 +0900

    update

---
+++ Partial_discount/EnumClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_discount
+{
+    public enum EnumItem
+    {
+        물,
+        음료수,
+        고기,
+        과일,
+        냉동식품,
+        즉석식품,
+        라면,
+    }
+
+    public enum EnumRate
+    {
+        할인_3 = 3,
+        할인_5 = 5,
+        할인_10 = 10,
+        할인_20 = 20,
+        사장할인_80 = 80,
+
+    }
+
+
+    class EnumClass
+    {
+    }
+}
---
+++ Partial_discount/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Partial_discount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*namespace가 같아서 EnumItem 불러오기 OK
+            EnumItem을 type형태로 가져옴. SysArray형식
+            but EnumItem[]은 배열형태 -> EnumItem배열 형태로 형변환을 해야함*/
+            EnumItem[] ei = (EnumItem[])Enum.GetValues(typeof(EnumItem));
+
+            foreach (EnumItem Eitem in ei)
+            {
+                cboxItem.Items.Add(Eitem.ToString());
+            }
+        }
+    }
+}
